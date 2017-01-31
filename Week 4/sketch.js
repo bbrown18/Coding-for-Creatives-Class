bc6b77b03c55419a879e815fbb43490a6db24080
Beverly Brown <beverlybrown@Beverlys-MacBook-Pro.local>
2017-01-31 13:57:34 -0500

    add week 4

---
+++ Week 4/sketch.js
@@ -0,0 +1,26 @@
+var rectx = 0
+
+function setup() {
+	createCanvas(640, 480);
+
+	// Your sketch code
+}
+
+function draw() {
+	background (230,230,250)
+	rect(rectx, 50, 200, 120);
+	rectx = rectx + 1
+	stroke(155,155,155)
+	fill(0,0,255,100)
+	ellipse(mousex,mousey,100,100);
+	fill(250,128,114)
+	line(350,300,450,550):
+	fill(50,205,50)
+
+
+
+	// Your sketch code
+}
+function mousepress(){
+	background(230,230,250)
+}
